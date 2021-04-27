a4ead6064ffdb9eb2510a528e54b14802ff5d046
ArtemVoychenko <80221393+ArtemVoychenko@users.noreply.github.com>
2021-04-27 18:59:11 +0300

    done createSlice 2

--- src/store/action-types.js
+++
@@ -1,4 +0,0 @@
-export const ActionTypes = {
-  RND: 'RND',
-  SET_INPUT_TEXT: 'SET_INPUT_TEXT'
-};
--- src/store/actions.js
+++
@@ -1,5 +0,0 @@
-import {ActionTypes} from './action-types';
-
-export const rndAction = (payload) => ({type: ActionTypes.RND, payload});
-export const setInputTextAction = (payload) => ({type: ActionTypes.SET_INPUT_TEXT, payload});
-
